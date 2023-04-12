33f58f1de243adb726c05d12cc1e0d0f2326a316
Shraddha09-Gupta22 <91560892+Shraddha09-Gupta22@users.noreply.github.com>
2023-04-12 19:00:51 +0530

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "🫶",
+    name: "Heart Hands",
+    meaning:
+      "Two hands forming a heart shape. Used to express love and support."
+  },
+  {
+    id: 2,
+    emoji: "😜",
+    name: "Winking Face with Tongue",
+    meaning:
+      "A yellow face sticking out its tongue and making a zany wink, usually shown with its right eye closed and left wide open. Often conveys a sense of fun, excitement, wackiness, buffoonery, or joking."
+  },
+  {
+    id: 3,
+    emoji: "🙂",
+    name: "Slightly Smiling Face",
+    meaning:
+      "A yellow face with simple, open eyes and a thin, closed smile. Conveys a wide range of positive, happy, and friendly sentiments. Its tone can also be patronizing, passive-aggressive, or ironic, as if saying This is fine when it’s really not."
+  },
+  {
+    id: 4,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 5,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 6,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  }
+];
+
+export default emojipedia;
